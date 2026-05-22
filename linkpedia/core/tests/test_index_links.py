1a284d48d9dde41ddbffc1d2de7c859656f96f4e
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-05-22 20:18:36 -0300

    TDD: testando o template redirecionado e a autenticação na rota index

--- linkpedia/core/tests/test_index_links.py
+++ linkpedia/core/tests/test_index_links.py
@@ -31,4 +31,36 @@ class LinkTest(TestCase):
         )
         # retornando o status code 200 se o usuário conseguir acessar autenticado
         self.assertEqual(response.status_code, 200)
-        
+    
+    def test_requires_login(self):
+
+        # Criando uma rota temporária
+        response = self.client.get(
+            reverse('table')
+        )
+
+        # Redirecionando o usuário que não possui login
+        self.assertEqual(response.status_code, 302)
+
+    def test_template(self):
+        # Criando um usuário temporário
+        user = User.objects.create_user(
+            username='aluno',
+            password='fatec'
+        )
+
+        # Logando o usuário temporáriamente
+        self.client.login(
+            username='aluno',
+            password='fatec'
+        )
+
+        # Criando o redirecionamento temporário
+        response = self.client.get(
+            reverse('table')
+        )
+
+        # Testando qual template é rediorecionado
+        self.assertTemplateUsed(
+            response, 'table.html'
+        )
